184305301fd96ef6dfe8f51227f51a55cb9a3e54
aman <aman.salimali@gmail.com>
2025-05-20 10:52:33 +0500

    Contact App -Using SQLITE -Created Contact Table -Insert Contact

---
+++ app/src/main/java/com/example/dbdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.dbdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.dbdemo.data.MyDBHandler;
+import com.example.dbdemo.model.Contact;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MyDBHandler db = new MyDBHandler(MainActivity.this);
+
+        //Creating contact
+        Contact aman = new Contact();
+        aman.setPhoneNumber("123456789012");
+        aman.setName("Aman");
+
+        //adding contact
+        db.addContact(aman);
+
+        //Creating contact
+        Contact aman1 = new Contact();
+        aman1.setPhoneNumber("123456789012");
+        aman1.setName("Aman22");
+
+        //adding contact
+        db.addContact(aman1);
+
+        //Creating contact
+        Contact aman2 = new Contact();
+        aman2.setPhoneNumber("123456789012");
+        aman2.setName("Aman11");
+
+        //adding contact
+        db.addContact(aman2);
+
+        Log.d("dbaman", "Id's are: " + aman.getId() + aman1.getId() + aman2.getId());
+
+
+    }
+}
---
+++ app/src/main/java/com/example/dbdemo/data/MyDBHandler.java
@@ -0,0 +1,54 @@
+package com.example.dbdemo.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.dbdemo.model.Contact;
+import com.example.dbdemo.params.Params;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    public MyDBHandler(Context context) {
+        super(context, Params.DB_NAME, null, Params.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "CREATE TABLE " + Params.TABLE_NAME +
+                "(" + Params.KEY_ID + " INTEGER PRIMARY KEY,"
+                + Params.KEY_NAME + " TEXT, "
+                + Params.KEY_PHONE + " TEXT" + ")";
+        Log.d("dbaman","QUERY BEING RUN IS : " + create);
+        db.execSQL(create);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Params.KEY_NAME, contact.getName());
+        values.put(Params.KEY_PHONE, contact.getPhoneNumber());
+
+        db.insert(Params.TABLE_NAME, null, values);
+        Log.d("dbaman", "Successfully inserted ");
+        db.close();
+    }
+
+//    public List<Contact> getAllContacts(){
+//        List<Contact> contactList = new ArrayList<>();
+//        SQLiteDatabase db = this.getReadableDatabase();
+//
+//        //genrate query to r
+//    }
+}
