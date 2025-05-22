a66e43a06715698f80fcd403f44c0a93a4eebd10
aman <aman.salimali@gmail.com>
2025-05-22 15:17:36 +0500

    -Delete Contact button Added.

--- app/src/main/java/com/example/dbdemo/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/dbdemo/adapter/RecyclerViewAdapter.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dbdemo.MainActivity;
 import com.example.dbdemo.R;
+import com.example.dbdemo.data.MyDBHandler;
 import com.example.dbdemo.displayContact;
 import com.example.dbdemo.model.Contact;
 
@@ -25,10 +27,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private Context context;
     private List<Contact> contactList;
+    private MyDBHandler dbHandler;
 
     public RecyclerViewAdapter(Context context, List<Contact> contactList) {
         this.context = context;
         this.contactList = contactList;
+        this.dbHandler = new MyDBHandler(context);
     }
 
     //where to get the single card as viewholder object
@@ -46,6 +50,25 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         holder.contactName.setText(contact.getName());
         holder.phoneNumber.setText(contact.getPhoneNumber());
+
+        holder.delBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int posi = holder.getAdapterPosition();
+                Contact contact = contactList.get(posi);
+
+                /*int id = contactList.get(posi).getId(); // Get actual DB ID
+                dbHandler.deleteContactById(id);*/
+
+                dbHandler.deleteContact(contact);
+
+                contactList.remove(posi);
+                notifyItemRemoved(posi);
+                notifyItemRangeChanged(posi, contactList.size());
+
+                Toast.makeText(context, "Deleted: " + contact.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     //how many items
@@ -58,6 +81,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public TextView contactName;
         public TextView phoneNumber;
         public ImageView iconButton;
+        public ImageButton delBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,6 +90,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             contactName = itemView.findViewById(R.id.rowName);
             phoneNumber = itemView.findViewById(R.id.rowContact);
             iconButton = itemView.findViewById(R.id.iconButton);
+            delBtn = itemView.findViewById(R.id.btnDelete);
 
             iconButton.setOnClickListener(this);
         }
